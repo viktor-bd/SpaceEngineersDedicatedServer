c1289c6928e4db7e108c7fd12295a384b68709b3
viktor-bd <viktordorph@hotmail.com>
2023-08-27 20:19:00 +0200

    Create ps script SetupSpaceEngineersDedicatedServer

---
+++ SetupSpaceEngineersDedicatedServer.ps1
@@ -0,0 +1,21 @@
+# Run PowerShell as Administrator
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
+
+# Install Chocolatey
+[System.Net.ServicePointManager]::SecurityProtocol = 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+$env:Path += ";$($env:ALLUSERSPROFILE)\chocolatey\bin"
+
+# Install software using Chocolatey
+choco install steamcmd -y
+choco install vcredist2013 -y
+choco install vcredist2017 -y
+
+# Install SE DS using steamCMD
+
+$steamCmdPath = "C:\ProgramData\chocolatey\lib\steamcmd\tools\steamcmd.exe"
+$installDir = "C:\SpaceEngineersDedicatedServer"
+
+Start-Process -FilePath $steamCmdPath -ArgumentList "+login anonymous +force_install_dir `"$installDir`" +app_update 298740 +quit"
+
+
